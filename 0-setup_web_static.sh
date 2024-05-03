7a407a8e996fb4d6c379b0f2307dd6b879a4d788
Yassine <yassinemoussafir31@gmail.com>
2024-05-03 08:36:51 -0700

    testing first task

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,35 +1,32 @@
 #!/usr/bin/env bash
-# This script sets up the web servers for the deployment of web_static.
+# This script sets up web servers for the deployment of web_static.
 
 # Install Nginx if it is not already installed
-sudo apt-get update
-sudo apt-get -y install nginx
+apt-get update
+apt-get -y install nginx
 
-# Create the required directories
-sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
+# Create required directories
+mkdir -p /data/web_static/releases/test /data/web_static/shared
 
 # Create a fake HTML file
-echo "<html>
+echo '<html>
   <head>
   </head>
   <body>
-    Testing Moussafir.tech Domain
+    Moussafir Domain
   </body>
-</html>" | sudo tee /data/web_static/releases/test/index.html > /dev/null
+</html>' > /data/web_static/releases/test/index.html
 
 # Create a symbolic link
-sudo ln -sf /data/web_static/releases/test /data/web_static/current
+ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-# Give ownership of the /data/ directory to the ubuntu user and group
-sudo chown -R ubuntu:ubuntu /data/
+# Change ownership
+chown -hR ubuntu:ubuntu /data/
 
-# Update Nginx configuration to serve the content
+# Update Nginx configuration to serve the content of /data/web_static/current/
 if ! grep -q "hbnb_static" /etc/nginx/sites-available/default; then
-    sudo sed -i '/server_name _;/a \\tlocation /hbnb_static/ { alias /data/web_static/current/; autoindex off; }' /etc/nginx/sites-available/default
+    sed -i '/server_name _;/a \\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}' /etc/nginx/sites-available/default
 fi
 
-# Restart Nginx to apply the configuration changes
-sudo service nginx restart
-
-# Exit successfully
-exit 0
+# Restart Nginx to apply changes
+service nginx restart
