95a61f739eceeb8fb608aede751add56a396c886
Yassine <yassinemoussafir31@gmail.com>
2024-05-03 10:05:33 -0700

    testing

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,47 +1,34 @@
 #!/usr/bin/python3
-"""Fabric script to deploy archive to web servers"""
+"""Fabric script to deploy archive to web servers using the function do_deploy."""
 
-from fabric.api import *
+from fabric.api import env, run, put
 from os.path import isfile
 
-env.hosts = ['54.237.7.136', '54.85.9.73']
-env.user = 'ubuntu'
+env.hosts = ['54.237.7.136', '54.85.9.73']  # IP addresses of web servers
+env.user = 'ubuntu'  # Username on the web servers
+env.key_filename = '~/.ssh/id_rsa'  # Private key for SSH
 
 def do_deploy(archive_path):
     if not isfile(archive_path):
         print("Archive path does not exist")
         return False
 
-    # Extracting file names
-    file_name = archive_path.split("/")[-1]
-    no_ext = file_name.split(".")[0]
-    
-    # Remote paths
-    remote_tmp_path = "/tmp/" + file_name
-    release_dir = "/data/web_static/releases/" + no_ext + "/"
+    file_name = archive_path.split('/')[-1]
+    base_name = file_name.split('.')[0]
+    remote_tmp_path = f"/tmp/{file_name}"
+    release_dir = f"/data/web_static/releases/{base_name}/"
 
     try:
-        # Upload the archive
-        put(archive_path, remote_tmp_path)
-        
-        # Uncompress the archive to the folder on the web server
-        run("mkdir -p " + release_dir)
-        run("tar -xzf " + remote_tmp_path + " -C " + release_dir)
-        
-        # Delete the archive from the web server
-        run("rm " + remote_tmp_path)
-        
-        # Move content to the parent directory
-        run("mv " + release_dir + "web_static/* " + release_dir)
-        
-        # Delete the symbolic link /data/web_static/current from the web server
-        run("rm -rf /data/web_static/current")
-        
-        # Create a new the symbolic link /data/web_static/current on the web server
-        run("ln -s " + release_dir + " /data/web_static/current")
-        
+        put(archive_path, remote_tmp_path)  # Upload the archive
+        run(f"sudo mkdir -p {release_dir}")
+        run(f"sudo tar -xzf {remote_tmp_path} -C {release_dir}")
+        run(f"sudo rm {remote_tmp_path}")
+        run(f"sudo mv {release_dir}web_static/* {release_dir}")
+        run(f"sudo rm -rf {release_dir}web_static")
+        run("sudo rm -rf /data/web_static/current")
+        run(f"sudo ln -s {release_dir} /data/web_static/current")
         print("New version deployed successfully!")
         return True
-    except:
-        print("Deployment failed")
+    except Exception as e:
+        print(f"Deployment failed: {e}")
         return False
