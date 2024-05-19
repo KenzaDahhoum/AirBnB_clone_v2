c58a322bc7ddb18f4b2c15bd046d5b800f9fd123
KenzaDahhoum <kenza.dahhoum@gmail.com>
2024-05-19 09:28:36 -0700

    3-python_route.py

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+""" Script to start a Flask web application with multiple routes """
+
+from flask import Flask
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello_world():
+    """ Returns 'Hello HBNB!' """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    """ Returns 'HBNB' """
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def c(text):
+    """ Returns 'C ' followed by the value of the text variable """
+    text = text.replace('_', ' ')
+    return 'C {}'.format(text)
+
+
+@app.route('/python/')
+@app.route('/python/<text>')
+def python(text='is cool'):
+    """ Returns 'Python ' followed by the value of the text variable """
+    text = text.replace('_', ' ')
+    return 'Python {}'.format(text)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+
