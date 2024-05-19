a3f0ca1871388052b0269072abcbee0e3a43ee85
KenzaDahhoum <kenza.dahhoum@gmail.com>
2024-05-19 09:49:37 -0700

    5-number_template.py and templates

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Add third view func that redirects and has default val for variable """
+""" 4. Add fourth view function that displays var only if is integer """
 
 from flask import Flask
 
@@ -33,6 +33,13 @@ def python_text(text='is cool'):
     """ replace more text with another variable. """
     text = text.replace('_', ' ')
     return 'Python {}'.format(text)
+
+
+@app.route('/number/<int:n>')
+def number_text(n):
+    """ replace with int only if given int. """
+    n = str(n)
+    return '{} is a number'.format(n)
 
 
 if __name__ == '__main__':
