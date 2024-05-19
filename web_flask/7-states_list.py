1c705764f4a0c6e41f585e6bc89ea54bf3cc60d8
KenzaDahhoum <kenza.dahhoum@gmail.com>
2024-05-19 10:20:39 -0700

    7-states_list.py

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,16 +1,27 @@
 #!/usr/bin/python3
-""" 7. Start flask service that does something. """
-
-from flask import Flask
-from flask import render_template
-
+"""
+Starts a Flask web application to display a list of states
+"""
+from flask import Flask, render_template
+from models import storage
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
-#@app.route()
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """Displays a list of states"""
+    states = storage.all("State").values()
+    sorted_states = sorted(states, key=lambda state: state.name)
+    return render_template('7-states_list.html', states=sorted_states)
 
 
-if __name__ == '__main__':
+@app.teardown_appcontext
+def teardown(exception):
+    """Closes the SQLAlchemy session"""
+    storage.close()
+
+
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
+
