96d061bcf32521e23d5a2cfc876e30599cd72985
KenzaDahhoum <kenza.dahhoum@gmail.com>
2024-05-19 09:44:04 -0700

    4-number_route.py

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-""" 
-4. Add fourth view function that displays var only if it is an integer 
-"""
+""" Add third view func that redirects and has default val for variable """
 
 from flask import Flask
 
@@ -24,23 +22,18 @@ def hello():
 
 @app.route('/c/<text>')
 def c_text(text):
-    """ Replace underscores with spaces in text. """
-    return 'C {}'.format(text.replace('_', ' '))
+    """ replace text with variable. """
+    text = text.replace('_', ' ')
+    return 'C {}'.format(text)
 
 
 @app.route('/python/')
 @app.route('/python/<text>')
 def python_text(text='is cool'):
-    """ Replace underscores with spaces in text. """
-    return 'Python {}'.format(text.replace('_', ' '))
-
-
-@app.route('/number/<int:n>')
-def number_text(n):
-    """ Display n is a number if n is an integer. """
-    return '{} is a number'.format(n)
+    """ replace more text with another variable. """
+    text = text.replace('_', ' ')
+    return 'Python {}'.format(text)
 
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-
